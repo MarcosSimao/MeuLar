2c429dbed266fd99b3e2ad895b4f0d74d1eee088
MarcosSimao <marcosjuremao@hotmail.com>
2020-09-23 00:04:30 -0300

    estabelecendo as classes cruds

---
+++ database/Delete.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Delete
+ *
+ * @author g624
+ */
+class Delete {
+    //put your code here
+}
---
+++ database/Insert.php
@@ -0,0 +1,4 @@
+<?php
+class Insert {
+    //put your code here
+}

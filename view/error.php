bc3f61cdbaeed79a6ea795a5b8de4ab0acc59dd4
MarcosSimao <marcosjuremao@hotmail.com>
2020-10-06 00:27:49 -0300

    metodos estaticos para procurar existencia de email,cpf e cnpj

---
+++ view/error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>PROCURE O ADM DA PAGINA ERRO EM ALGUM EXCEPTION < E MOSTRE AONDE VC ENCONTROU O ERRO </h1>
+    </body>
+</html>

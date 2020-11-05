000de126fc16cfc9c4c001611d360514e7db65db
MarcosSimao <marcosjuremao@hotmail.com>
2020-11-04 22:44:52 -0200

    refatorando codigo

---
+++ view/pages/testeListar/listarImagem.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+$img = "../../../uploadImg/homer/0.jpg";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <img src="<?php echo $img  ?>"/>
+    </body>
+</html>
